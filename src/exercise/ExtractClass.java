0bc68d1647055d26496c74474d323f8a3ad142be
nathabuddhi <nathabuddhi@gmail.com>
2025-02-13 16:37:00 +0700

    Fixed ExtractClass

--- src/exercise/ExtractClass.java
+++ src/exercise/ExtractClass.java
@@ -3,26 +3,47 @@ public class ExtractClass {
 // todo extract class TelephoneNumber from class Person
 	class Person {
 	  private String name;
-	  private String officeAreaCode;
-	  private String officeNumber;
+	  private TelephoneNumber telephoneNumber = new TelephoneNumber();
 
 	  public String getName() {
-		return name;
-	  }
-	  public String getTelephoneNumber() {
-		return ("(" + officeAreaCode + ") " + officeNumber);
-	  }
-	  public String getOfficeAreaCode() {
-		return officeAreaCode;
-	  }
-	  public void setOfficeAreaCode(String arg) {
-		officeAreaCode = arg;
-	  }
-	  public String getOfficeNumber() {
-		return officeNumber;
-	  }
-	  public void setOfficeNumber(String arg) {
-		officeNumber = arg;
-	  }
+          return name;
+      }
+
+      public void setName(String name) {
+          this.name = name;
+      }
+
+      public String getTelephoneNumber() {
+          return telephoneNumber.getTelephoneNumber();
+      }
+
+      public TelephoneNumber getTelephone() {
+          return telephoneNumber;
+      }
 	}
+	
+	class TelephoneNumber {
+        private String officeAreaCode;
+        private String officeNumber;
+
+        public String getTelephoneNumber() {
+            return "(" + officeAreaCode + ") " + officeNumber;
+        }
+
+        public String getOfficeAreaCode() {
+            return officeAreaCode;
+        }
+
+        public void setOfficeAreaCode(String officeAreaCode) {
+            this.officeAreaCode = officeAreaCode;
+        }
+
+        public String getOfficeNumber() {
+            return officeNumber;
+        }
+
+        public void setOfficeNumber(String officeNumber) {
+            this.officeNumber = officeNumber;
+        }
+    }
 }
